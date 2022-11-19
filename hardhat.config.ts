c916ac43a5b1664b7e31827e389c6d85487626b4
Merdi-kim <merdikimuha1@gmail.com>
2022-11-19 11:56:12 +0300

    add providers

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+const fs = require("fs");
+const privateKey = fs.readFileSync(".secret").toString();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
-    
+    mumbai: {
+      url: 'https://polygon-mumbai.g.alchemy.com/v2/LhltBAHnspBMIgCx1SsxYJqM_rJOeZYe',
+      accounts: [`0x${privateKey}`]
+    }
   }
 };
 
